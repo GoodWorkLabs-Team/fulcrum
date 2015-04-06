d2e6d03839733e53099a418fb2f8ebea0f2a957c
Rajib Chowdhury <chowdhury284@gmail.com>
2015-04-06 14:03:01 +0530

    default configs changed.

--- config/fulcrum_defaults.rb
+++ config/fulcrum_defaults.rb
@@ -1,12 +1,12 @@
 Configuration.for('fulcrum') do
   # On Heroku, ensure you `heroku config:add APP_HOST=appname.herokuapp.com`
-  app_host ENV['APP_HOST'] || '127.0.0.1:3000'
+  app_host ENV['APP_HOST'] || '128.199.204.78:3000'
 
   # The address which system emails will originate from.
-  mailer_sender ENV['MAILER_SENDER'] || 'noreply@example.com'
+  mailer_sender ENV['MAILER_SENDER'] || 'fulcrum@goodworklabs.com'
 
   # Disable registration pages
-  disable_registration ENV['DISABLE_REGISTRATION'] || false
+  disable_registration ENV['DISABLE_REGISTRATION'] || true
 
   # Project column order:
   # progress_to_right: chilly bin, backlog, in progress, done
