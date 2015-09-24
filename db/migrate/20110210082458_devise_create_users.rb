a414c001d97f301f9f80be94518d970e905a5740
Rajib Chowdhury <chowdhury284@gmail.com>
2015-09-24 17:09:03 +0530

    db migrations updated.

--- db/migrate/20110210082458_devise_create_users.rb
+++ db/migrate/20110210082458_devise_create_users.rb
@@ -1,12 +1,28 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def self.up
     create_table(:users) do |t|
-      t.database_authenticatable :null => false
-      t.recoverable
-      t.rememberable
-      t.trackable
-      t.confirmable
-      t.encryptable
+      t.string   "email",                              default: "",   null: false
+      t.string   "encrypted_password",     limit: 128, default: "",   null: false
+      t.string   "reset_password_token"
+      t.string   "remember_token"
+      t.datetime "remember_created_at"
+      t.integer  "sign_in_count",                      default: 0
+      t.datetime "current_sign_in_at"
+      t.datetime "last_sign_in_at"
+      t.string   "current_sign_in_ip"
+      t.string   "last_sign_in_ip"
+      t.string   "confirmation_token"
+      t.datetime "confirmed_at"
+      t.datetime "confirmation_sent_at"
+      t.string   "password_salt"
+      t.datetime "reset_password_sent_at"
+      
+      # t.database_authenticatable :null => false
+      # t.recoverable
+      # t.rememberable
+      # t.trackable
+      # t.confirmable
+      # t.encryptable
       # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
       # t.token_authenticatable
 
